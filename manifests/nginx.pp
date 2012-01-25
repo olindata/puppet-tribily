6c5539603a3fd88f70594969b38719dfcb1a2c93
Daniel Black <daniel.black@openquery.com>
2012-01-25 02:32:38 -0500

    fix nginx so it works

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,19 +1,25 @@
-
 class tribily::nginx {
 
-  $nginx_script = /usr/local/sbin/nginx_status.sh
+  $nginx_script = '/usr/local/sbin/nginx_status.sh'
 
-  tribily::agent::userparams { "${name}-userparams":
-    userparams => [
-      "nginx.accepts,${nginx_script} accepts",
-      "nginx.handled,${nginx_script} handled",
-      "nginx.requests,${nginx_script} requests",
-      "nginx.connections.active,${nginx_script} active",
-      "nginx.connections.reading,${nginx_script} reading",
-      "nginx.connections.writing,${nginx_script} writing",
-      "nginx.connections.waiting,${nginx_script} waiting",
-
-    ]
+  file{ "${tribily::params::userparam_conf_dir}/nginx.conf":
+    ensure  => 'present',
+    mode    => 0640,
+    owner   => $tribily::params::agent_user,
+    group   => $tribily::params::agent_user,
+    content => 
+      "nginx.accepts,${nginx_script} accepts
+nginx.handled,${nginx_script} handled
+nginx.requests,${nginx_script} requests
+nginx.connections.active,${nginx_script} active
+nginx.connections.reading,${nginx_script} reading
+nginx.connections.writing,${nginx_script} writing
+nginx.connections.waiting,${nginx_script} waiting",
+    require => [
+      User[$tribily::params::agent_user],
+      File[$tribily::params::userparam_conf_dir],
+    ],
+    notify  => Service["zabbix-agent"]
   }
 
 
